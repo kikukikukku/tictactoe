1c6ab9083380bb238555ed3d14fe836adfe6b95b
LinJuanmao <2211741172@qq.com>
2020-01-02 13:29:07 +0800

    Updates

---
+++ precache-manifest.3d8724e86ce98ca81c034625eacaba7e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ddb0cbfc7e5b1422670db5ca9204b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db7494ce2668f97f6b39",
+    "url": "/static/js/2.c4263f20.chunk.js"
+  },
+  {
+    "revision": "a19aa8908b5fff36bf95",
+    "url": "/static/js/main.e79c16ad.chunk.js"
+  },
+  {
+    "revision": "455497e10b16cbebba49",
+    "url": "/static/js/runtime-main.67950c1a.js"
+  }
+]);
